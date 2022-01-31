905e2e26c1395470bc80c75f3388b48d7fe69105
Kathy Kim <kitkathy5192@gmail.com>
2022-01-31 04:09:08 -0500

    2.9

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -5,19 +5,39 @@
 //  Created by Kathy Kim on 1/24/22.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var awesomeLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        awesomeLabel.text = "You are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        
+        print(imageNumber)
+        //let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10 {
+            imageNumber=0
+        }
+//        if awesomeLabel.text=="You are Awesome!" {
+//            awesomeLabel.text = "You are Great!"
+//            imageView.image = UIImage(named: "image3")
+//        } else if awesomeLabel.text == "You are Great!" {
+//            awesomeLabel.text = "You Are Da Bomb!"
+//            imageView.image = UIImage(named: "image2")
+//        } else {
+//            awesomeLabel.text = "You are Awesome!"
+//            imageView.image = UIImage(named:"image0")
+//        }
     }
     
 }
