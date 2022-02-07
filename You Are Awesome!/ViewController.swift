8c6ce20ac0c730992c8ae671c0fef9973cff4d03
Kathy Kim <kitkathy5192@gmail.com>
2022-02-07 03:58:14 -0500

    Finished 2.13

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var awesomeLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,27 +28,21 @@ class ViewController: UIViewController {
                         "You've got the Design Skills of Jony Ive!",
                         "When the Genius Bar Needs Help, They call You!"]
         
-        awesomeLabel.text = messages[Int.random(in: 0...messages.count-1)]
-        imageView.image = UIImage(named: "image\(Int.random(in: 0...9))")
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber == newMessageNumber
+        messageNumber = newMessageNumber
+        awesomeLabel.text = messages[newMessageNumber]
+        
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0...totalNumberOfImages)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        imageView.image = UIImage(named: "image\(imageNumber)")
 
        
-        //let imageName = "image" + String(imageNumber)
-//        let imageName = "image\(imageNumber)"
-//        imageView.image = UIImage(named: imageName)
-//        imageNumber = imageNumber + 1
-//        if imageNumber == 10 {
-//            imageNumber=0
-//        }
-//        if awesomeLabel.text=="You are Awesome!" {
-//            awesomeLabel.text = "You are Great!"
-//            imageView.image = UIImage(named: "image3")
-//        } else if awesomeLabel.text == "You are Great!" {
-//            awesomeLabel.text = "You Are Da Bomb!"
-//            imageView.image = UIImage(named: "image2")
-//        } else {
-//            awesomeLabel.text = "You are Awesome!"
-//            imageView.image = UIImage(named:"image0")
-//        }
     }
     
 }
